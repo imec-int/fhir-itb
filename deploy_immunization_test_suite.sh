1f927833a1cbfd345c035c8450465b9bb58641b8
Gwen Bleyen <gwenbleyen@shiflteftconsulting.io>
2024-11-20 14:05:37 +0100

    Additional tests

---
+++ deploy_immunization_test_suite.sh
@@ -0,0 +1,6 @@
+echo zip test suite...
+cd test-suites
+zip -r immunization-suite.zip immunization-suite
+
+curl -F updateSpecification=true -F specification=01FAE6F4XC022X4FE3XBE11X1293E8F73C23 -F testSuite=@immunization-suite.zip --header "ITB_API_KEY: F6628973X0EE7X40ECX9C36X8384C71F63D2" -X POST http://localhost:9000/api/rest/testsuite/deploy
+rm -f immunization-suite.zip
