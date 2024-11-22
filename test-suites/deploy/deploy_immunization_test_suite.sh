cc38ef7bc6b30394a34cfd57ab4462ab3299e0d4
Bernardo J. C. Nunes <bernardo.nunes@imec.be>
2024-11-22 17:15:05 +0100

    feat: updated immunization tests

--- test-suites/deploy/deploy_immunization_test_suite.sh
+++ test-suites/deploy/deploy_immunization_test_suite.sh
@@ -1,5 +1,5 @@
 echo zip test suite...
-cd test-suites
+cd ..
 zip -r immunization-suite.zip immunization-suite
 
 curl -F updateSpecification=true -F specification=01FAE6F4XC022X4FE3XBE11X1293E8F73C23 -F testSuite=@immunization-suite.zip --header "ITB_API_KEY: F6628973X0EE7X40ECX9C36X8384C71F63D2" -X POST http://localhost:9000/api/rest/testsuite/deploy
